b385a3cb327a7b8b74ba4dab0fcfa88a6fc1175b
wuwood <kilokingw@gmail.com>
2016-01-22 10:04:34 +0800

    masonry

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require masonry/jquery.masonry
 //= require turbolinks
 //= require_tree .
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'pages/info'
 
   resources :ideas
-  root to: redirect('/ideas')
+  root to: 'ideas#index'
   get "pages/info"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
