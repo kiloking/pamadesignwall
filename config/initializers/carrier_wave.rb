5331ab83875ba4cce809a1416727452cfe8caac9
wuwood <kilokingw@gmail.com>
2016-02-03 22:33:46 +0800

    s3 key

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,11 +2,11 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV['AKIAJEW3KOLSPDO2SUIQ'],
-      aws_secret_access_key: ENV['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'],
+      aws_access_key_id:     ENV['S3_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET'],
       region:                'ap-northeast-1'
     }
-    config.fog_directory  = ENV['pamadesign']
+    config.fog_directory  = ENV['S3_BUCKET']
   else
     config.storage :file
   end
